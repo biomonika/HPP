8ddcff5dea3f4889dc4b8f1238169b1f07d24834
Monika Cechova <mcechova@ucsc.edu>
2023-04-10 17:27:19 -0700

    format as soon as the first iteration is finished

--- StSat/annotate_StSat_for_species.sh
+++ StSat/annotate_StSat_for_species.sh
@@ -11,7 +11,6 @@ assembly_name="${assembly%.*}"
 myThreads=10
 
 conda activate /public/home/mcechova/conda/ncrf
-
 echo "Using set of "
 awk '{print NF}' StSat.txt | sort -nu | tail -n 1
 echo " motifs."
@@ -28,12 +27,19 @@ python parse_ncrf_to_find_new_motifs.py NCRF.${assembly_name}.raw1.ncrf #adds th
 conda activate /public/home/mcechova/conda/ncrf
 ncrf_cat NCRF.${assembly_name}.raw1.ncrf | ncrf_summary --minmratio=0.85 >${assembly_name}.summary1.txt
 ncrf_resolve_overlaps ${assembly_name}.summary1.txt >${assembly_name}.overlaps1.txt
+awk '{print $3 "\t" $4 "\t" $5 "\t" $2 "\t" $9 "\t" $6}' ${assembly_name}.overlaps1.txt.unique.entries.txt >NCRF.${assembly_name}.iteration1.bed
+cat NCRF.${assembly_name}.iteration1.bed | awk -F'\t' 'BEGIN{SUM=0}{ SUM+=$3-$2 }END{print SUM}'
+
+conda activate /public/home/mcechova/conda/python.3.10
+#only report the overlapping alignment with the highest mRatio
+python report_highest_scoring_motif_per_overlapping_block.py ${assembly_name}.overlaps1.txt
 
 #convert into one motif per line, so that only unique motifs can be kept
 cat StSat.txt | tr ' ' '\n' >StSat.tmp
 cat NCRF.${assembly_name}.raw1.ncrf.ncrf.new.motifs.txt StSat.tmp | sort | uniq | tr '\n' ' ' >${assembly_name}.extendedStSat.txt #concatenated original motifs and the extended motifs
 rm StSat.tmp
 
+conda activate /public/home/mcechova/conda/ncrf
 echo "Using set of "
 awk '{print NF}' ${assembly_name}.extendedStSat.txt | sort -nu | tail -n 1
 echo " motifs."
@@ -48,9 +54,8 @@ ncrf_resolve_overlaps ${assembly_name}.summary2.txt >${assembly_name}.overlaps2.
 
 conda activate /public/home/mcechova/conda/python.3.10
 #only report the overlapping alignment with the highest mRatio
-python report_highest_scoring_motif_per_overlapping_block.py ${assembly_name}.overlaps1.txt
 python report_highest_scoring_motif_per_overlapping_block.py ${assembly_name}.overlaps2.txt
-awk '{print $3 "\t" $4 "\t" $5 "\t" $2 "\t" $9 "\t" $6}' ${assembly_name}.overlaps2.txt.unique.entries.txt >NCRF.${assembly_name}.bed
-awk '{print $3 "\t" $4 "\t" $5 "\t" $2 "\t" $9 "\t" $6}' ${assembly_name}.overlaps2.txt.unique.entries.txt >NCRF.${assembly_name}.bed
-cat NCRF.${assembly_name}.bed | awk -F'\t' 'BEGIN{SUM=0}{ SUM+=$3-$2 }END{print SUM}'
+
+awk '{print $3 "\t" $4 "\t" $5 "\t" $2 "\t" $9 "\t" $6}' ${assembly_name}.overlaps2.txt.unique.entries.txt >NCRF.${assembly_name}.iteration2.bed
+cat NCRF.${assembly_name}.iteration2.bed | awk -F'\t' 'BEGIN{SUM=0}{ SUM+=$3-$2 }END{print SUM}'
 echo "Done."
