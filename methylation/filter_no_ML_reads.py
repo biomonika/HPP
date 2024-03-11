7c3b8c01e3d6205023aaefd595206dbf2ae69a58
Monika Cechova <mcechova@ucsc.edu>
2024-03-11 12:45:13 -0700

    Create filter_no_ML_reads.py

---
+++ methylation/filter_no_ML_reads.py
@@ -0,0 +1,26 @@
+# This script separates reads to two bam files: those that contain and do not contain Ml tag
+# 2024 Monika Cechova mcechova@ucsc.edu
+
+import sys
+import pysam
+
+filename = sys.argv[1]
+samfile = pysam.AlignmentFile(filename, "rb")
+output_filtered = pysam.AlignmentFile(filename+".onlyMLTags.bam", "wb", template=samfile)
+output_missing = pysam.AlignmentFile(filename+".missingTags.bam", "wb", template=samfile)
+
+counter=0
+for read in samfile:
+    #print(read.query_name)
+    try:
+        ml_tag = read.get_tag("Ml")
+        output_filtered.write(read)
+    except KeyError:
+        #print("The 'Ml' tag was not found.")
+        output_missing.write(read)
+        counter+=1
+
+
+samfile.close()
+print("Done. Output file created. The number of reads with missing Ml tags: ", counter)
+print("Please modify the script if you wish to filter ML and not Ml tags. ")
