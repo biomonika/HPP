dc8295d28e1d970c2a7e8913aeb6171666e1f194
Monika Cechova <mcechova@ucsc.edu>
2024-04-02 19:03:19 -0700

    Update swap_acrocentrics.sh

--- patching/swap_acrocentrics.sh
+++ patching/swap_acrocentrics.sh
@@ -39,44 +39,55 @@ assembly_to_be_patched_censat_track="full.maternal.contigs.cenSat.bed"
 assembly_reference="duplex.maternal.contigs.fa"
 assembly_reference_mashmap="mashmap/duplex.maternal.contigs.mashmap.txt"
 assembly_reference_censat_track="duplex.maternal.contigs.cenSat.bed"
-chromosome="chr21"
 
 assembly_to_be_patched_name=$(basename -- "$assembly_to_be_patched")
 assembly_to_be_patched_name="${assembly_to_be_patched_name%.*}"
 assembly_reference_name=$(basename -- "$assembly_reference")
 assembly_reference_name="${assembly_reference_name%.*}"
 
-#find where rDNA coordinates start
-contig_name_assembly_to_be_patched=`grep ${chromosome} ${assembly_to_be_patched_mashmap} | cut -d' ' -f1`
-contig_length_assembly_to_be_patched=`grep ${chromosome} ${assembly_to_be_patched_mashmap} | cut -d' ' -f2`
-contig_name_assembly_reference=`grep ${chromosome} ${assembly_reference_mashmap} | cut -d' ' -f1`
-contig_length_assembly_reference=`grep ${chromosome} ${assembly_reference_mashmap} | cut -d' ' -f2`
+acrocentric_chromosome=("chr13" "chr14" "chr15" "chr21" "chr22")
 
-#we need to find rDNA of length at least 1kb, and that's when we stop
-#this first part of the assembly will be ignored
-#even if we find multiple rDNA hits, we are only interested in the first one of sufficient length
-rDNA_start_assembly_to_be_patched=`cat ${assembly_to_be_patched_censat_track} | egrep ${contig_name_assembly_to_be_patched} | egrep "rDNA" | bedtools sort | bedtools merge | awk '$3 - $2 > 5000' | head -n 1 | cut -f2 `
-rDNA_start_assembly_reference=`cat ${assembly_reference_censat_track} | egrep ${contig_name_assembly_reference} | egrep "rDNA" | bedtools sort | bedtools merge | awk '$3 - $2 > 5000' | head -n 1 | cut -f2 `
+swap_chromosome() {
+	chromosome=$1
 
-#only keep part of the assembly AFTER the first rDNA
-region=${contig_name_assembly_to_be_patched}:${rDNA_start_assembly_to_be_patched}-${contig_length}
-echo ${region}
-samtools faidx ${assembly_to_be_patched} ${region} >${assembly_to_be_patched_name}.toKeep.fa
+	#find where rDNA coordinates start
+	contig_name_assembly_to_be_patched=`grep ${chromosome} ${assembly_to_be_patched_mashmap} | cut -d' ' -f1`
+	contig_length_assembly_to_be_patched=`grep ${chromosome} ${assembly_to_be_patched_mashmap} | cut -d' ' -f2`
+	contig_name_assembly_reference=`grep ${chromosome} ${assembly_reference_mashmap} | cut -d' ' -f1`
+	contig_length_assembly_reference=`grep ${chromosome} ${assembly_reference_mashmap} | cut -d' ' -f2`
+
+	#we need to find rDNA of length at least 1kb, and that's when we stop
+	#this first part of the assembly will be ignored
+	#even if we find multiple rDNA hits, we are only interested in the first one of sufficient length
+	rDNA_start_assembly_to_be_patched=`cat ${assembly_to_be_patched_censat_track} | egrep ${contig_name_assembly_to_be_patched} | egrep "rDNA" | bedtools sort | bedtools merge | awk '$3 - $2 > 5000' | head -n 1 | cut -f2 `
+	rDNA_start_assembly_reference=`cat ${assembly_reference_censat_track} | egrep ${contig_name_assembly_reference} | egrep "rDNA" | bedtools sort | bedtools merge | awk '$3 - $2 > 5000' | head -n 1 | cut -f2 `
+
+	#only keep part of the assembly AFTER the first rDNA
+	region=${contig_name_assembly_to_be_patched}:${rDNA_start_assembly_to_be_patched}-${contig_length}
+	echo ${region}
+	samtools faidx ${assembly_to_be_patched} ${region} >${assembly_to_be_patched_name}.toKeep.fa
 
 
-#only keep part of the assembly BEFORE the first rDNA
-region=${contig_name_assembly_reference}:0-${rDNA_start_assembly_reference}
-echo ${region}
-samtools faidx ${assembly_reference} ${region} >${assembly_reference_name}.toKeep.fa
+	#only keep part of the assembly BEFORE the first rDNA
+	region=${contig_name_assembly_reference}:0-${rDNA_start_assembly_reference}
+	echo ${region}
+	samtools faidx ${assembly_reference} ${region} >${assembly_reference_name}.toKeep.fa
 
-#combine both assemblies
-cat ${assembly_reference_name}.toKeep.fa ${assembly_to_be_patched_name}.toKeep.fa >${chromosome}.${assembly_to_be_patched_name}.acroswap.fa
-#remove unnecessary files
-ls ${assembly_to_be_patched_name}.toKeep.fa
-ls ${assembly_reference_name}.toKeep.fa
-rm ${assembly_to_be_patched_name}.toKeep.fa ${assembly_reference_name}.toKeep.fa
+	#combine both assemblies
+	cat ${assembly_reference_name}.toKeep.fa ${assembly_to_be_patched_name}.toKeep.fa >${chromosome}.${assembly_to_be_patched_name}.acroswap.fa
+	#remove unnecessary files
+	ls ${assembly_to_be_patched_name}.toKeep.fa
+	ls ${assembly_reference_name}.toKeep.fa
+	rm ${assembly_to_be_patched_name}.toKeep.fa ${assembly_reference_name}.toKeep.fa
 
-echo "After acroswap, the resulting patched file is: " ${chromosome}.${assembly_to_be_patched_name}.acroswap.fa
+	echo "After acroswap, the resulting patched file is: " ${chromosome}.${assembly_to_be_patched_name}.acroswap.fa
+}
+
+for chromosome in "${acrocentric_chromosome[@]}"; do
+    echo "$chromosome"
+    swap_chromosome $chromosome
+done
+
 echo "Done."
 date
 
